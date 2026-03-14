46d209e36af74adeeb8352bfa91b99224f7f20ca
fruscars <2979313819@qq.com>
2026-03-14 15:41:16 +0800

    Update config.py

--- qa/PythonProject2/config.py
+++ qa/PythonProject2/config.py
@@ -9,7 +9,7 @@ CONNECTION_NAME="default"
 #neo4j的连接
 NEO4J_URI = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "wm12345678..."      # 请修改为您的实际密码
+NEO4J_PASSWORD = ""      # 请修改为您的实际密码
 
 #知识图谱api(智谱)chat类
 KNWEB_API_KEY = ""  # 请确保有效
